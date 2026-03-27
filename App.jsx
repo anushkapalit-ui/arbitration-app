9261611878a72ef4e398e2c2a04ffc3dba7c0a0e
anushkapalit-ui <anushka.palit@gmail.com>
2026-03-27 18:12:07 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,8 @@
 import ArbitrationSimulator from './ArbitrationSimulator'
 export default function App() {
-  return <ArbitrationSimulator />
-}
+  return (
+  <div style={{ padding: '20px', background: 'blue', color: 'white' }}>
+    <h1>The App is working!</h1>
+    <ArbitrationSimulator />
+  </div>
+)
